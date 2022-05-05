a897c9e103f4b03352024c368a81b91a667fc6ce
VladimirKozemaslov <kvs@intelins.ru>
2022-05-05 12:00:28 +0500

    7. Метод удаления заявки Tracker.delete [#211749 #297105]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -61,8 +61,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            int start = index + 1;
-            System.arraycopy(items, start, items, index, size - index - 1);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
