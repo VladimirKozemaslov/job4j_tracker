b696c5685b34e90483f22a38698e9567a7a3e791
VladimirKozemaslov <kvs@intelins.ru>
2022-05-12 18:12:23 +0500

    0.4. Пользовательские исключения. [#219367 #300078]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,7 +4,6 @@ import java.util.Objects;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
         for (int i = 0; i < value.length; i++) {
             if (Objects.equals(key, value[i])) {
                 return i;
