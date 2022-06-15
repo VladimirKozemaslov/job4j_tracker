e7dc517f868857f5bf3901eb04e792064990e700
VladimirKozemaslov <kvs@intelins.ru>
2022-06-15 11:30:08 +0500

    2. Список адресов [#110062 #312204]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,7 +7,8 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream()
+        return
+                profiles.stream()
                 .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
